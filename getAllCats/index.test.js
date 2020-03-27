bf1afcf12d0c7df465080b64525a376f0401876f
Marius CONJEAUD <m.conjeaud@groupeonepoint.com>
2020-03-27 14:25:56 +0100

    Add test for getCat function

--- getAllCats/index.test.js
+++ getAllCats/index.test.js
@@ -1,12 +1,13 @@
 const httpFunction = require('./index');
 const context = require('../testing/defaultContext')
 
-test('Http trigger should return known text', async () => {
-
-    const request = {};
+describe('API', () => {
+    it('#getAllCats should return known objects', async () => {
+        const request = {};
 
     await httpFunction(context, request);
 
     expect(context.log.mock.calls.length).toBe(1);
     expect(context.res.body).toEqual([{ name: 'lilly' }, { name: 'lucy' }]);
+    });
 });
